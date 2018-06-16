d24b548362fe11121927ed084d267dcafbb58d16
Donovan Venuto <tronovan@gmail.com>
2018-06-16 14:42:12 -0500

    Formatting.

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const app =express();
+const app = express();
 // index route 
+
+const port = 4200;
+
 app.get('/', (req, res) => {
+
     res.send('INDEX');
 });
 
-const port = 4200;
+app.get('/about', (req, res) => {
+
+    res.send('ABOUT');
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-});
-
-// about route
-app.get('/about', (req, res) => {
-    res.send('About');
-});
-
-// hello route
-app.get('/hello', (req, res) => {
-    res.send('hello');
 });
